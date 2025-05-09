bc343d7037f92883e6e3d7232d13d95869f198e9
akrawczyk1 <71944688+akrawczyk1@users.noreply.github.com>
2025-05-09 12:25:38 -0400

    Completes MVP for etchsketch project
    -Adds requirement for mouse button to be held
    in order to draw.
    -Remembers canvas size when canvas is reset.
    TODO:
    -Input validation for number of pixels per side
    -Rainbow mode
    -Eraser

--- script.js
+++ script.js
@@ -1,30 +1,83 @@
-const container = document.querySelector(".sketch-container");
 
-const containerSize = 960;
-let sideLength = 16;
+function resetSketchpad(container, sideLength){
 
-let count = 0;
+    container.replaceChildren();
 
-for (let i = 0; i < sideLength; i++){
-    const sketchNodeRow = document.createElement("div");
-    sketchNodeRow.classList.add("sketch-container", "flex");
+    const containerSizePixels = container.offsetHeight;
 
-    for (let j = 0; j < sideLength; j++){
-        const sketchNode = document.createElement("div");
-        sketchNode.classList.add("white", "sketch-node");
-        sketchNode.style.height = ((containerSize / sideLength) - 2) + "px";
-        sketchNode.style.width = ((containerSize / sideLength) - 2) + "px";
-        sketchNode.addEventListener("mouseenter", (event) => {
-            event.target.classList.remove("white");
-            event.target.classList.add("black");
-        })
-        sketchNodeRow.appendChild(sketchNode);
+    for (let i = 0; i < sideLength; i++){
+
+        const sketchNodeRow = document.createElement("div");
+        sketchNodeRow.classList.add("sketch-container", "flex");
+    
+        for (let j = 0; j < sideLength; j++){
+    
+            const sketchNode = document.createElement("div");
+            sketchNode.classList.add("white", "sketch-node");
+            sketchNode.style.height = ((containerSizePixels / sideLength) - 2) + "px";
+            sketchNode.style.width = ((containerSizePixels / sideLength) - 2) + "px";
+            sketchNode.addEventListener("mouseenter", (event) => {
+                if (mouseDown){
+                    event.target.classList.remove("white");
+                    event.target.classList.add("black");
+                }
+            })
+    
+            sketchNodeRow.appendChild(sketchNode);
+        }
+    
+        container.appendChild(sketchNodeRow);
     }
 
-    container.appendChild(sketchNodeRow);
+    console.log("End of resetSketchpad");
+
 }
 
+function resizeSketchpad(container, resizeText, containerSizeSquares){
 
-console.log(count);
+    const newSize = parseInt(resizeText.value);
+    if (newSize <= 0) return;
+
+    sessionStorage.setItem("numPixelsPerSide", newSize);
+
+    resetSketchpad(container, newSize);
+    resizeText.value = "";
+
+}
+
+// Initialize size of the canvas
+const STARTING_SIZE_PIXELS = 10;
+sessionStorage.setItem("numPixelsPerSide", "10");
+
+
+// Grab references to relevant elements
+const container = document.querySelector(".sketch-container");
+const resetButton = document.querySelector(".reset");
+const resizeButton = document.querySelector(".resize-button");
+const resizeText = document.querySelector(".resize-text");
+
+// Track mouse button status
+let mouseDown = false;
+
+document.addEventListener("mousedown", () => {mouseDown = true;});
+document.addEventListener("mouseup", () => {mouseDown = false;});
+
+// Functionality for clearing the canvas but not changing the size
+resetButton.addEventListener("click", () => {
+    const numPixelsPerSide = parseInt(sessionStorage.getItem("numPixelsPerSide"));
+    resetSketchpad(container, numPixelsPerSide)
+});
+
+// Functionality for clearing the canvas and changing the size
+resizeButton.addEventListener("click", () => {
+    resizeSketchpad(container, resizeText);
+});
+
+// Initialize the canvas
+resetSketchpad(container, STARTING_SIZE_PIXELS);
+
+
+
+
 
 
